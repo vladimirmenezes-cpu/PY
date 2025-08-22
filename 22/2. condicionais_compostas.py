62fa9912dc1e3c9634c6480d7fcfc625ebee3bf1
vladimirmenezes-cpu <vladimir.menezes@ba.estudante.senai.br>
2025-08-22 16:51:52 -0300

    Add files via upload

---
+++ 22/2. condicionais_compostas.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls")
+
+
+idade = 20
+if idade < 18:
+    print ("Menor de idade")
+else:
+    print("Maior de idade")
+    
+    
---
+++ 22/exercicio.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+numero1 = int(input("Me informe um numero: "))
+
+if numero1 > 10:
+    print ("É MAIOR QUE 10")
+elif numero1 == 10:
+    print("o numero é igual a 10")
+elif numero1 < 10:
+    print("É MENOR QUE 10")
+    
+    print("Fim")
