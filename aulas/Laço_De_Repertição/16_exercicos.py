e95c93d88ac7f0b2baf689626b9c8f19e05dcc8c
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-12 14:52:50 -0300

    .\aulas\

--- aulas/Laço_De_Repertição/16_exercicos.py
+++ aulas/Laço_De_Repertição/16_exercicos.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import time 
 st.title("Laço de reptição - For")
-st.header("MEDIA DE 4 NOTAS")
+st.header("MEDIA DE 3 NOTAS")
 
 media = 0
 soma = 0
-for i in range(1,5):
+for i in range(1,4):
     numero = st.number_input(f"Digite a sua nota {i}:", step=1)
     soma = soma + numero 
-    media = soma  / 4
+    media = soma  / 3
     st.balloons()
 
 if st.button("Verificar"):
+    if media >= 7:
+        st.success(f" APROVADO! Parabens sua media foi {media}")
+    elif media >= 4:
+        st.warning(f"RECU RECU : {media}")
+    else:
+        st.error("REPROVADO BURRAO TU")
     st.success(f"Sua media é: {media}")
     
